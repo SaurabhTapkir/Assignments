2b0161876227f265ffe67399e059be8eb490544b
SaurabhTapkir <144579093+SaurabhTapkir@users.noreply.github.com>
2023-09-24 23:09:10 +0530

    Increment and Decrement Program
    Use to Build Logic and Trace The Program

---
+++ inde.java
@@ -0,0 +1,9 @@
+class inde{
+    public static void main(String[] args) {
+        int a=6;
+        int b=3; 
+        int c=2;
+       int result=(a*b)+(c-1)/(b%c);
+       System.out.println(result);
+    }
+}
---
+++ inde4.java
@@ -0,0 +1,11 @@
+public class inde4 {
+        public static void main(String[] args)
+    {
+        int p=10;
+        int q=p++-3;
+        int r=--q + ++p;
+        int s=p--*q++ +--r;
+        System.out.println(s             );
+
+   }
+}
